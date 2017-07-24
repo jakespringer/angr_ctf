411c4290c0b03dc69a977c902fdcaa5593aa7a72
Jake Springer <me@jakespringer.com>
2017-07-24 11:40:31 -0700

    Edit solve.py to account for refactoring

--- solve.py
+++ solve.py
@@ -8,7 +8,7 @@ def main():
   proj = angr.Project(sys.argv[1])
   initial_state = proj.factory.entry_state()
   pg = proj.factory.path_group(initial_state, veritesting=False)
-  pg.explore(find=lambda p: 'Success.' in p.state.posix.dumps(1))
+  pg.explore(find=lambda p: 'Good Job.' in p.state.posix.dumps(1))
   print pg
   print repr(pg.found[0].state.posix.dumps(0))
 
