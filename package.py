81df63d4de174cfda1d36b5e9c34327a6d10e60a
Jake Springer <me@jakespringer.com>
2017-07-25 16:57:08 -0700

    Fix a few bugs that prevented compilation

--- package.py
+++ package.py
@@ -23,6 +23,7 @@ def package_level(level_name, output_base_directory, num_binaries, user, salt, e
     extra_file_abs = os.path.join('.', level_name, extra_file)
     extra_file_target = os.path.join(output_base_directory, level_name, extra_file)
     shutil.copyfile(extra_file_abs, extra_file_target)
+  print('Compiled %s.' % (level_name,))
 
 def package_all(root_folder):
   num_binaries = 1
