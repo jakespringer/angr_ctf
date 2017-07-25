f2b24a0c86b9cb267de07b586accdb4d4329bbdc
Jake <me@jakespringer.com>
2017-07-25 14:29:54 -0700

    Improve clarity for 02

--- 02_angr_find_condition/scaffold02.py
+++ 02_angr_find_condition/scaffold02.py
@@ -1,3 +1,15 @@
+# It is very useful to be able to search for a path that reaches a certain
+# instruction. However, in some cases, you may not know the address of the
+# specific instruction you want to reach (or perhaps there is no single
+# instruction goal.) In this challenge, you don't know which instruction
+# grants you success. Instead, you just know that you want to find a path where
+# the binary prints "Good Job."
+#
+# Angr is powerful in that it allows you to search for a path that meets an
+# arbitrary condition that you specify in Python, using a predicate you define
+# as a function that takes a path and returns True if you have found the path
+# you are looking for, and False otherwise.
+
 import angr
 import sys
 
@@ -7,7 +19,8 @@ def main(argv):
   initial_state = project.factory.entry_state()
   path_group = project.factory.path_group(initial_state)
 
-  # Define a function that checks if a given path successfully found a backdoor.
+  # Define a function that checks if a given path successfully found the path
+  # you are looking for.
   def is_successful(path):
     # Dump whatever has been printed out by the binary so far into a string.
     stdout_output = path.state.posix.dumps(sys.stdout.fileno())
@@ -16,7 +29,10 @@ def main(argv):
     # (!)
     return ???  # :boolean
 
-  # Same as above, but this time check if the path should abort.
+  # Same as above, but this time check if the path should abort. If you return
+  # False, Angr will continue to step the path. In this specific challenge, the
+  # only time at which you will know you should abort is when the program prints
+  # "Try again."
   def should_abort(path):
     stdout_output = path.state.posix.dumps(sys.stdout.fileno())
     return ???  # :boolean
