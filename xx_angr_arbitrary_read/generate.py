8532f7ebae20a2d02e2f471767522ed2f5e9ac42
Jake Springer <me@jakespringer.com>
2017-08-02 11:22:50 -0700

    Fix angr_arbitrary_read

--- xx_angr_arbitrary_read/generate.py
+++ xx_angr_arbitrary_read/generate.py
@@ -23,7 +23,7 @@ def generate(argv):
   with tempfile.NamedTemporaryFile(delete=False, suffix='.c') as temp:
     temp.write(c_code)
     temp.seek(0)
-    os.system('gcc -m32 -o ' + output_file + ' ' + temp.name)
+    os.system('gcc -m32 -fno-stack-protector -Wl,--section-start=.rodata=0x34343434 -o ' + output_file + ' ' + temp.name)
 
 if __name__ == '__main__':
   generate(sys.argv)
