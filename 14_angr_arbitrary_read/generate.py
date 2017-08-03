03b20a6125372cdc747596cc41d828c0e7ce5195
Jake Springer <me@jakespringer.com>
2017-08-03 09:32:10 -0700

    Reorder + refactor, commit 2/2

--- 14_angr_arbitrary_read/generate.py
+++ 14_angr_arbitrary_read/generate.py
@@ -17,7 +17,7 @@ def generate(argv):
   with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'description.txt'), 'r') as desc_file:
     description = desc_file.read().encode('string_escape').replace('\"', '\\\"')
 
-  template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'xx_angr_arbitrary_read.c.templite'), 'r').read()
+  template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '14_angr_arbitrary_read.c.templite'), 'r').read()
   c_code = Templite(template).render(description=description)
 
   with tempfile.NamedTemporaryFile(delete=False, suffix='.c') as temp:
--- package.py
+++ package.py
@@ -39,11 +39,12 @@ def package_all(root_folder):
   package_level('08_angr_constraints', root_folder, num_binaries, root_folder, year, ['scaffold08.py'])
   package_level('09_angr_hooks', root_folder, num_binaries, root_folder, year, ['scaffold09.py'])
   package_level('10_angr_sim_procedures', root_folder, num_binaries, root_folder, year, ['scaffold10.py'])
-  package_level('11_angr_locate_vulnerable', root_folder, num_binaries, root_folder, year, ['scaffold11.py'])
-  package_level('12_angr_unconstrained', root_folder, num_binaries, root_folder, year, ['scaffold12.py'])
-  package_level('13_angr_veritesting', root_folder, num_binaries, root_folder, year, ['scaffold13.py'])
-  package_level('14_angr_static_binary', root_folder, num_binaries, root_folder, year, ['scaffold14.py'])
-  package_level('15_angr_shared_library', root_folder, num_binaries, root_folder, year, ['scaffold15.py'])
+  package_level('11_angr_veritesting', root_folder, num_binaries, root_folder, year, ['scaffold11.py'])
+  package_level('12_angr_static_binary', root_folder, num_binaries, root_folder, year, ['scaffold12.py'])
+  package_level('13_angr_shared_library', root_folder, num_binaries, root_folder, year, ['scaffold13.py'])
+  package_level('14_angr_arbitrary_read', root_folder, num_binaries, root_folder, year, ['scaffold14.py'])
+  package_level('15_angr_arbitrary_write', root_folder, num_binaries, root_folder, year, ['scaffold15.py'])
+  package_level('16_angr_arbitrary_jump', root_folder, num_binaries, root_folder, year, ['scaffold16.py'])
 
 if __name__ == '__main__':
   if len(sys.argv) != 2:
