3ba70854b2b3c4e4f7bdc8d8a056d7ff14c83795
Wu-chang Feng <wuchang@pdx.edu>
2017-08-10 22:00:33 +0000

    typos

--- solutions/09_angr_hooks/scaffold09.py
+++ solutions/09_angr_hooks/scaffold09.py
@@ -32,7 +32,7 @@ def main(argv):
   # bytes are used to represent them in memory. This will be the skip length.
   # (!)
   instruction_to_skip_length = ???
-  @angr.Hook(check_equals_called_address, length=instruction_to_skip_length)
+  @project.hook(check_equals_called_address, length=instruction_to_skip_length)
   def skip_check_equals_(state):
     # Determine the address where user input is stored. It is passed as a
     # parameter ot the check_equals_ function. Then, load the string. Reminder:
@@ -57,7 +57,7 @@ def main(argv):
     state.regs.eax = claripy.If(
       user_input_string == check_against_string, 
       claripy.BVV(1, 32), 
-      claripy.BVV(1, 32)
+      claripy.BVV(0, 32)
     )
 
   simulation = project.factory.simgr(initial_state)
