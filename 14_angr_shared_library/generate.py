5f1fc3d74944ed99e57e3c3830050c2f45ee82d0
Mark Mankins <mark.mankins@ngc.com>
2018-03-16 04:57:47 -0400

    Update build script for 14 so the resulting binary can locate the shared library at runtime.

--- 14_angr_shared_library/generate.py
+++ 14_angr_shared_library/generate.py
@@ -26,7 +26,7 @@ def generate(argv):
     os.system('gcc -I' + os.path.dirname(os.path.realpath(__file__))  + ' -fno-stack-protector -fpic -m32 -c -o 14_angr_shared_library.o ' + temp.name)
     os.system('gcc -shared -m32 -o ' + os.path.join('/'.join(output_file.split('/')[0:-1]), 'lib' + output_file.split('/')[-1] + '.so') + ' 14_angr_shared_library.o')
     os.system('rm 14_angr_shared_library.o')
-    os.system('chmod -x ' + os.path.join('/'.join(output_file.split('/')[0:-1]), 'lib' + output_file.split('/')[-1] + '.so'))
+    os.system('chmod -x ' + os.path.dirname(os.path.realpath(__file__)) + '/lib' + output_file.split('/')[-1] + '.so')
 
   template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '14_angr_shared_library.c.templite'), 'r').read()
   c_code = Templite(template).render()
@@ -34,7 +34,7 @@ def generate(argv):
   with tempfile.NamedTemporaryFile(delete=False, suffix='.c') as temp:
     temp.write(c_code)
     temp.seek(0)
-    os.system('gcc -m32 -I . -L ' + '/'.join(output_file.split('/')[0:-1]) + ' -o ' + output_file + ' ' + temp.name + ' -l' + output_file.split('/')[-1])
+    os.system('gcc -m32 -I ' + os.path.dirname(os.path.realpath(__file__)) + ' -Xlinker -R' + os.path.dirname(os.path.realpath(__file__)) + ' -L' + os.path.dirname(os.path.realpath(__file__)) + ' -o ' + output_file + ' ' + temp.name + ' -l' + output_file.split('/')[-1])
 
 if __name__ == '__main__':
   generate(sys.argv)
