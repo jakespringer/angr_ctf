312e7146e638ca44afff12ee6c060df7a59cad2b
Your Name <you@example.com>
2019-03-18 19:36:30 +0000

    fixes non-capital letter in address issue

--- 16_angr_arbitrary_write/generate.py
+++ 16_angr_arbitrary_write/generate.py
@@ -13,9 +13,9 @@ def generate(argv):
 
   random.seed(seed)
 
-  rodata_tail_modifier = 0x2e
-  rodata_parts = ''.join([ chr(random.randint(ord('A'), ord('Z'))) for _ in xrange(3) ]
-    + [ chr(random.randint(ord('A') - rodata_tail_modifier, ord('Z') - rodata_tail_modifier)) ])
+  rodata_tail_modifier = 0x15
+  rodata_parts = ''.join([ chr(random.randint(ord('A'), ord('Z'))) for _ in xrange(3) ] + [ chr(random.randint(0,4) + rodata_tail_modifier) ])
+
   rodata_address = '0x' + rodata_parts.encode('hex')
 
   userdef_charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
