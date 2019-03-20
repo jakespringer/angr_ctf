4f7bceb81062f48040713ef2411d053d552e60bb
Your Name <you@example.com>
2019-03-20 04:57:49 +0000

    fix non capital letter

--- 15_angr_arbitrary_read/generate.py
+++ 15_angr_arbitrary_read/generate.py
@@ -13,9 +13,8 @@ def generate(argv):
 
   random.seed(seed)
   
-  rodata_tail_modifier = 0x13 
-  rodata_parts = ''.join([ chr(random.randint(ord('A'), ord('Z'))) for _ in xrange(3) ] 
-    + [ chr(random.randint(ord('A') - rodata_tail_modifier, ord('Z') - rodata_tail_modifier)) ])
+  rodata_tail_modifier = 0x30 
+  rodata_parts = ''.join([ chr(random.randint(ord('A'), ord('Z'))) for _ in xrange(3) ] + [ chr(random.randint(0,16) + rodata_tail_modifier) ])
   rodata_address = '0x' + rodata_parts.encode('hex')
 
   description = ''
