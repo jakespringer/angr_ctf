213a7cb3f1c962eb91cd7cc5fea010ca1bae906c
Gilad Reich <giladreich@users.noreply.github.com>
2021-08-25 22:48:16 +0200

    Add a note to fix deprecation on ctf_12 for get_any_node.
    This prints to stdout unfortunately:
    Deprecation warning: Use self.model.get_any_node() instead of get_any_node

--- solutions/12_angr_veritesting/solver.py
+++ solutions/12_angr_veritesting/solver.py
@@ -1,3 +1,4 @@
+# TODO: FIXME deprecation
 # When you construct a simulation manager, you will want to enable Veritesting:
 # project.factory.simgr(initial_state, veritesting=True)
 # Hint: use one of the first few levels' solutions as a reference.
