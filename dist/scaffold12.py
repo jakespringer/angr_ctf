c252126137cbc422846cb28828bd10b8f4bc3470
Gilad Reich <giladreich@users.noreply.github.com>
2021-08-25 18:23:38 +0200

    Remove dist directory.
    These artifacts are duplicated under the solutions directory and should be recompiled in order for the offsets to be different per distribution.

--- dist/scaffold12.py
+++
@@ -1,3 +0,0 @@
-# When you construct a simulation manager, you will want to enable Veritesting:
-# project.factory.simgr(initial_state, veritesting=True)
-# Hint: use one of the first few levels' solutions as a reference.
