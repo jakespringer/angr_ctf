4d8bbee27f70af13a65f16ad5b0d2f5232616f7b
Gilad Reich <giladreichgr@gmail.com>
2021-09-04 16:34:12 +0200

    Add pdf version of the presentation and fix unzipping on Windows.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ docker build --platform linux/amd64 -t angr_ctf .
 container_id=$(docker create angr_ctf)
 docker cp "$container_id":/home/angr_ctf/ctfs - > angr_ctfs.tar
 docker rm -v "$container_id"
-tar -xzf angr_ctfs.tar
+tar -xvf angr_ctfs.tar
 rm angr_ctfs.tar
