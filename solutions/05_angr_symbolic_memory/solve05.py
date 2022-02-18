32e9c3cae398ae3a4184daa20f542fe351acfebb
Wu-chang Feng <wuchang@pdx.edu>
2022-02-18 07:55:35 -0800

    05 updated compiler

--- solutions/05_angr_symbolic_memory/solve05.py
+++ solutions/05_angr_symbolic_memory/solve05.py
@@ -6,7 +6,7 @@ def main(argv):
   path_to_binary = argv[1]
   project = angr.Project(path_to_binary)
 
-  start_address = 0x8048606
+  start_address = 0x8048618
   initial_state = project.factory.blank_state(addr=start_address)
 
   # The binary is calling scanf("%8s %8s %8s %8s").
@@ -21,13 +21,13 @@ def main(argv):
   # 'value' (a bitvector) to 'address' (a memory location, as an integer.) The
   # 'address' parameter can also be a bitvector (and can be symbolic!).
   # (!)
-  password0_address = 0xa29faa0
+  password0_address = 0xb6f44c0
   initial_state.memory.store(password0_address, password0)
-  password1_address = 0xa29faa8
+  password1_address = 0xb6f44c8
   initial_state.memory.store(password1_address, password1)
-  password2_address = 0xa29fab0
+  password2_address = 0xb6f44d0
   initial_state.memory.store(password2_address, password2)
-  password3_address = 0xa29fab8
+  password3_address = 0xb6f44d8
   initial_state.memory.store(password3_address, password3)
 
 
