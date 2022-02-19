8b5d66fd3f08533e7418d3afb8c4c4927b72846d
Wu-chang Feng <wuchang@pdx.edu>
2022-02-19 10:39:29 -0800

    14 updated compiler

--- solutions/14_angr_shared_library/solve14.py
+++ solutions/14_angr_shared_library/solve14.py
@@ -14,10 +14,10 @@ def main(argv):
   # base + offset, where offset is their address in the file.
   # (!)
   base = 0x4000000
-  project = angr.Project(path_to_binary, load_options={ 
-    'main_opts' : { 
-      'custom_base_addr' : base 
-    } 
+  project = angr.Project(path_to_binary, load_options={
+    'main_opts' : {
+      'base_addr' : base
+    }
   })
 
   # Initialize any needed values here; you will need at least one to pass to
@@ -27,15 +27,15 @@ def main(argv):
   # Begin the state at the beginning of the validate function, as if it was
   # called by the program. Determine the parameters needed to call validate and
   # replace 'parameters...' with bitvectors holding the values you wish to pass.
-  # Recall that 'claripy.BVV(value, size_in_bits)' constructs a bitvector 
+  # Recall that 'claripy.BVV(value, size_in_bits)' constructs a bitvector
   # initialized to a single value.
   # Remember to add the base value you specified at the beginning to the
   # function address!
   # Hint: int validate(char* buffer, int length) { ...
   # Another hint: the password is 8 bytes long.
   # (!)
-  validate_function_address = base + 0x6d7
-  initial_state = project.factory.call_state(validate_function_address, buffer_pointer, claripy.BVV(8, 32))
+  validate_function_address = base + 0x670
+  initial_state = project.factory.call_state(validate_function_address, buffer_pointer, claripy.BVV(8, 32), add_options = { angr.options.SYMBOL_FILL_UNCONSTRAINED_MEMORY, angr.options.SYMBOL_FILL_UNCONSTRAINED_REGISTERS})
 
   # You will need to add code to inject a symbolic value into the program. Also,
   # at the end of the function, constrain eax to equal true (value of 1) just
@@ -48,14 +48,14 @@ def main(argv):
 
   simulation = project.factory.simgr(initial_state)
 
-  success_address = base + 0x783
+  success_address = base + 0x71c
   simulation.explore(find=success_address)
 
   if simulation.found:
     solution_state = simulation.found[0]
 
     solution_state.add_constraints(solution_state.regs.eax != 0)
-  
+
     # Determine where the program places the return value, and constrain it so
     # that it is true. Then, solve for the solution and print it.
     # (!)
