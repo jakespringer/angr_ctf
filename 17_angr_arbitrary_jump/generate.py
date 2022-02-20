f6023a45270be9d0ab54366c59ce51a9733fdf39
Wu-chang Feng <wuchang@pdx.edu>
2022-02-20 06:55:37 -0800

    fix offset of last byte to stay in ASCII

--- 17_angr_arbitrary_jump/generate.py
+++ 17_angr_arbitrary_jump/generate.py
@@ -15,7 +15,7 @@ def generate(argv):
 
 # cs492
 #  text_tail_modifier0 = 0x05
-  text_tail_modifier0 = 0x10
+  text_tail_modifier0 = 0x40
   text_tail_modifier1 = 0x01
   text_parts = ''.join([ chr(random.randint(ord('A'), ord('Z'))) for _ in range(2) ]
     + [ chr(random.randint(ord('A') - text_tail_modifier1, ord('Z') - text_tail_modifier1)) ]
